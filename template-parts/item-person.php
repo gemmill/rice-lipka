ece508b72b7480f92189bafb4acdef612ea01704
Aaron Gemmill <63611552+gemmillaru@users.noreply.github.com>
2026-03-10 00:06:47 -0400

    style

--- template-parts/item-person.php
+++ template-parts/item-person.php
@@ -14,11 +14,11 @@ $person_title = get_the_title();
 $person_fields = ricelipka_get_post_type_fields();
 
 // Component classes - can be customized via args
-$component_class = isset($args['class']) ? $args['class'] : 'person-item';
-$image_size = isset($args['image_size']) ? $args['image_size'] : 'large';
+$component_class = 'item-person';
+$image_size = 'large';
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class($component_class . ' person-item'); ?>>
+<article id="item-<?php the_ID(); ?>" <?php post_class($component_class); ?>>
     
     <h3><?php echo $person_title ?></h3>
 
@@ -45,11 +45,9 @@ $image_size = isset($args['image_size']) ? $args['image_size'] : 'large';
 
 
 
-        
-
-            <div class="person-content">
+    
                 <?php the_content(); ?>
-            </div>
+  
         
     </div>
     
